025a7654e99745c5a0ecd1938c843981ea4e3209
snehagang <snehagang25@gmail.com>
2024-11-21 22:17:29 +0530

    first changes

---
+++ TheNotebook-main/src/components/App.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+
+function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes((prevValue) => {
+      return [...prevValue, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((note, index) => {
+        return id !== index;
+      });
+    });
+  }
+
+  return (
+    <div>
+      <CreateArea onAdd={addNote} />
+      {notes.map((note, index) => (
+        <Note
+          key={index}
+          id={index}
+          title={note.title}
+          content={note.content}
+          onDelete={deleteNote}
+        />
+      ))}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
